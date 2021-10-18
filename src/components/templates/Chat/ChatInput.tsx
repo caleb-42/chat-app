f203ceb31ca2135ea71fa38e2a1bec9ecc0685e0
caleb-42 <ewisco316@yahoo.com>
2021-10-18 09:59:22 +0100

    complete date command

--- src/components/templates/Chat/ChatInput.tsx
+++ src/components/templates/Chat/ChatInput.tsx
@@ -14,18 +14,19 @@ export const ChatInput = () => {
 
 	const [msg, setMsg] = useState('');
 	const emitMsg = (e: any) => {
-		if (!msg) return;
 		e.preventDefault();
+		if (!msg) return;
 		const data = { author: user?.username, message: msg } as IMessage;
 		dispatch(addMsg(data));
 		socket?.emit('message', data);
+		setMsg('');
 	}
 
 	return <form onSubmit={emitMsg}>
 		<Box w="100%" display="flex" alignItems="center">
 			<CTextField value={msg} onChange={(e) => setMsg(e.target.value)} width="100%" />
 			<Box ml="1rem">
-				<IconButton type="submit" borderRadius="60px" h="60px" w="60px" colorScheme="primary" aria-label="send">
+				<IconButton _hover={{ bg: 'primary.600' }} type="submit" borderRadius="60px" h="60px" w="60px" bg='primary.400' aria-label="send">
 					<Image src={Assets.SEND_MSG} />
 				</IconButton>
 			</Box>
